5bfd263971311d54cd58cef3437c86b7000eeaf8
Huang Chi-Han <huangchiheng@gmail.com>
2019-11-08 09:13:26 +0800

    Add more comments to the tests

--- prototypes.test.js
+++ prototypes.test.js
@@ -1,30 +1,53 @@
 describe('javascript function / object various traits', () => {
   test('where is __proto__ property?', () => {
+    // Object has __proto__ property
     const a = {
       name: 'Bryan Huang'
     }
     expect(a).toHaveProperty('__proto__')
 
+    // Function has __proto__ property
     function Human () {}
     expect(Human).toHaveProperty('__proto__')
 
+    // Array has __proto__ property
     const testArr = []
     expect(testArr).toHaveProperty('__proto__')
   })
 
   test('ok, it seem like object, function and array they all have __proto__ property, but what are they pointing to?', () => {
+
+    // The __proto__ property of Function is pointing to Function prototype.
     expect(Function.__proto__).toEqual(Function.prototype)
 
+    // The __proto__ property of Object is pointing to Object prototype
     const a = {
       name: 'Bryan Huang'
     }
     expect(a.__proto__).toEqual(Object.prototype)
 
+    // A named function's __proto__ property is pointing to Function prototype
     function Human () {}
     expect(Human.__proto__).toEqual(Function.prototype)
 
+    // An Array's __proto__ property is pointing to Array prototype
     const testArr = []
     expect(testArr.__proto__).toEqual(Array.prototype)
+  })
+
+  test('prototype linkage', () => {
+    function foo (name) {
+      this.name = name
+    }
+
+    // __proto__ indicates prototype linkage
+    console.log(foo.prototype.__proto__.constructor)
+
+    var a = new foo('bryan')
+    console.log(a.__proto__.constructor === foo)
+    console.log(a.__proto__ === foo.prototype)
+    console.log(a.name)
+    console.log(a.hasOwnProperty('name'))
   })
 
   test('what about \'prototype\' property? where are they?', () => {
@@ -38,12 +61,15 @@ describe('javascript function / object various traits', () => {
 
     const testArr = []
     expect(testArr.prototype).toBeUndefined()
+
+    // Object and array does not have prototype property. Named function has a prototype property.
   })
 
   test('now I understand function can have both __proto__ and prototype. what about function constructor? what are they? and where are they', () => {
     // The constructor of a function is pointing to Function.constructor
     function Human () {}
-    expect(Human.constructor).toEqual(Function.constructor)
+    expect(Human.constructor).toEqual(Function)
+    expect(Human.prototype.constructor).toEqual(Human)
 
     // The constructor of object a equals to 'Object' function
     // It seems like object literal is created by 'Object' function
@@ -54,7 +80,24 @@ describe('javascript function / object various traits', () => {
 
     // Object constructor is pointing to Function constructor
     // since Object itself is a constructor.
-    expect(Object.constructor).toEqual(Function.constructor)
+    expect(Object.constructor).toEqual(Function)
+
+    expect(Function.constructor).toEqual(Function)
+
+    // So the named function "Human" has constructor property on itself and prototype.
+    // The constructor on "Human" function is pointing to Function constructor whereas
+    // the constructor on "prototype" property is pointing to Human itself
+    //
+    //   Human.constructor ---> Function.constructor
+    //   Human.prototype.constructor ---> Human
+    //
+    // Since object literal is an instantiation from Object function, it's constructor is pointing to object function
+    //
+    //   a.constructor ---> Object
+    //
+    // Object function's constructor is pointing to Function
+    //
+    //   Object.constructor ---> Function.constructor
   })
 
   xtest('what is the difference between __proto__ and prototype', () => {
