9ce3a22c9c036a4f9749cbea4b7d86172a85a2c3
Huang Chi-Han <huangchiheng@gmail.com>
2019-11-09 14:07:55 +0800

    Added prototype linkage examples.

--- prototypes.test.js
+++ prototypes.test.js
@@ -17,37 +17,55 @@ describe('javascript function / object various traits', () => {
 
   test('ok, it seem like object, function and array they all have __proto__ property, but what are they pointing to?', () => {
 
-    // The __proto__ property of Function is pointing to Function prototype.
     expect(Function.__proto__).toEqual(Function.prototype)
-
-    // The __proto__ property of Object is pointing to Object prototype
     const a = {
       name: 'Bryan Huang'
     }
     expect(a.__proto__).toEqual(Object.prototype)
 
-    // A named function's __proto__ property is pointing to Function prototype
     function Human () {}
     expect(Human.__proto__).toEqual(Function.prototype)
 
-    // An Array's __proto__ property is pointing to Array prototype
     const testArr = []
     expect(testArr.__proto__).toEqual(Array.prototype)
+
+    // __proto__ property is also known as "prototype linkage". The __proto__ property is an accessor to "prototype" property.
+    //
+    //  - The __proto__ property of Function is pointing to Function prototype.
+    //  - The __proto__ property of Object is pointing to Object prototype
+    //  - A named function's __proto__ property is pointing to Function prototype
+    //  - An Array's __proto__ property is pointing to Array prototype
   })
 
-  test('prototype linkage', () => {
+  test('The behavior of __proto__, prototype linkage and the \'new\' keyword', () => {
     function foo (name) {
       this.name = name
     }
 
-    // __proto__ indicates prototype linkage
-    console.log(foo.prototype.__proto__.constructor)
+    // foo's __proto__ is pointing to Function's prototype object
+    // foo.__proto__ ---> Function.prototype
+    // foo.prototype.constructor ---> foo
+    expect(foo.__proto__).toEqual(Function.prototype)
+    expect(foo.prototype.__proto__).toEqual(Object.prototype) // foo and Object is prototypal inherited via prototype linkage "__proto__"
+    expect(foo.prototype.__proto__.constructor).toEqual(Object.prototype.constructor)
+    expect(foo.prototype.__proto__.constructor).toEqual(Object)
+    expect(foo.prototype.constructor).toEqual(foo)
 
-    var a = new foo('bryan')
-    console.log(a.__proto__.constructor === foo)
-    console.log(a.__proto__ === foo.prototype)
-    console.log(a.name)
-    console.log(a.hasOwnProperty('name'))
+    var bar = new foo('Bryan')
+    expect(bar.__proto__).toEqual(foo.prototype)
+    expect(bar.prototype).toBeUndefined()
+
+    // notice that "bar" does not have a "constructor" property. Javascript engine searches for "constructor"
+    // through the protype chain. "constructor" is not found in bar itself, js engine continue searching
+    // foo's prototype since "bar" and "foo" are now connected via prototype linkage '__proto__'. "constructor"
+    // is found in foo's prototype.
+    expect(bar.constructor).toBe(foo)
+    expect(bar.__proto__.constructor).toBe(foo)
+    expect(bar.hasOwnProperty('name')).toBeTruthy()
+
+    var baz = new foo('Jason')
+    expect(baz.__proto__).toEqual(bar.__proto__)
+    expect(baz.__proto__).toEqual(foo.prototype)
   })
 
   test('what about \'prototype\' property? where are they?', () => {
@@ -81,17 +99,17 @@ describe('javascript function / object various traits', () => {
     // Object constructor is pointing to Function constructor
     // since Object itself is a constructor.
     expect(Object.constructor).toEqual(Function)
-
     expect(Function.constructor).toEqual(Function)
 
-    // So the named function "Human" has constructor property on itself and prototype.
-    // The constructor on "Human" function is pointing to Function constructor whereas
-    // the constructor on "prototype" property is pointing to Human itself
+    // So the named function "Human" has "constructor" property on itself and it's prototype.
     //
-    //   Human.constructor ---> Function.constructor
+    //   Human.constructor ---> Function
     //   Human.prototype.constructor ---> Human
     //
-    // Since object literal is an instantiation from Object function, it's constructor is pointing to object function
+    // The constructor on prototype of "Human" function is pointing to Function constructor whereas
+    // the constructor on "prototype" property is pointing back to Human
+    //
+    // Since object literal is an instantiation from Object function, it's constructor on prototype object is pointing to Object function
     //
     //   a.constructor ---> Object
     //
@@ -100,41 +118,26 @@ describe('javascript function / object various traits', () => {
     //   Object.constructor ---> Function.constructor
   })
 
-  xtest('what is the difference between __proto__ and prototype', () => {
-    // The __proto__ getter function exposes the value of the internal [[Prototype]] of an object.
-    // For objects created using an object literal, this value is Object.prototype:
-    const a = {
-      name: 'Bryan Huang'
-    }
-    expect(a.__proto__).toEqual(Object.prototype)
-    expect(typeof a.__proto__).toBe('object')
-
-    // object do not have prototype:
-    expect(a.prototype).toBeUndefined()
-
-    // For objects created using array literals, the value of __proto__ is Array.prototype
-    const testArr = []
-    expect(testArr.__proto__).toEqual(Array.prototype)
-
-    // object has __proto__ but not prototype, where as function has both prototype and __proto__.
-    // we are expecting testFunc to have the following properties:
-    //   testFunc
-    //     - firstName
-    //     - lastName
-    //     - prototype
-    //     - __proto__
-    //     - constructor
-    function testFunc (firstName, lastName) {
-      this.firstName = firstName
-      this.lastName = lastName
+  test('what is shadowing?', () => {
+    function Human (name) {
+      this.name = name
     }
 
-    // constructor function "testFunc" has both "prototype" and __proto__. they are both objects.
-    // For function, the vlaue of __proto__ is Function.prototype
-    expect(testFunc.prototype).toEqual({})
-    expect(testFunc.__proto__).toEqual(Function.prototype)
+    // added an method on prototype object
+    Human.prototype.sayName = function () {
+      console.log('my name on prototype object' + this.name)
+    }
 
-    // the prototype of "testFunc" contains 2 properties, constructor and __proto__.
-    // "constructor" is pointing to testFunc function itself
+    var a = new Human('Bryan')
+    a.sayName()
+
+    // added another method on Human itself
+    a.sayName = function () {
+      console.log('my name on object' + this.name)
+    }
+
+    a.sayName()
+    
+    // "sayName" method on Object "a" is shadowing "sayName" method in "prototype" object.
   })
 })
